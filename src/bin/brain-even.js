2b6230a15a0f8ca87d4b71771cdbe48a705b58fb
Kir Kup <k1r.bu@yandex.ru>
2019-01-16 12:56:38 +0300

    correct even

--- src/bin/brain-even.js
+++ src/bin/brain-even.js
@@ -1,27 +1,5 @@
 #!/usr/bin/env node
-import readlineSync from 'readline-sync';
+import evenGame from '../games/even';
 
-const isEven = num => num % 2 === 0;
-const evenGame = () => {
-  console.log('Welcome to the Brain Games!');
-  console.log('Answer "yes" if number even otherwise answer "no"');
-  const userName = readlineSync.question('May I have your name? ');
-  console.log(`Hello, ${userName}!`);
-  let count = 0;
-  while (count !== 3) {
-    const randomNum = Math.floor(Math.random() * 1000);
-    console.log(`Question: ${randomNum}`);
-    const answer = readlineSync.question('Your answer: ');
-    const correctAnswer = answer === 'yes' ? 'no' : 'yes';
-    const check = isEven(randomNum) ? 'yes' : 'no';
-    if (answer === check) {
-      console.log('Correct!');
-    } else {
-      console.log(`${answer} is wrong answer ;(. Correct answer was '${correctAnswer}'.`);
-      console.log(`Let's try again, ${userName}`);
-    }
-    count += 1;
-  }
-  console.log(`Congratulations, ${userName}`);
-};
+
 evenGame();
