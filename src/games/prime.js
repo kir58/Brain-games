7dbf34cffc67964d2880db8ed0a8545d103bf4f3
Kir Kup <k1r.bu@yandex.ru>
2019-01-17 15:50:40 +0300

    step 8 prime

---
+++ src/games/prime.js
@@ -0,0 +1,25 @@
+import gameFlow from '..';
+import generateNum from '../utils';
+import { cons } from 'hexlet-pairs';
+
+const description = 'Answer "yes" if given number is prime. Otherwise answer "no"';
+
+const isPrime = (num) => {
+  if (num < 2) {
+    return false;
+  }
+  for (let i = 2; i < num; i += 1) {
+    if (num % i === 0) {
+      return false;
+    }
+  }
+  return true;
+};
+
+const primeGame = () => {
+  const question = generateNum(1, 100);
+  const answer = isPrime(question) ? 'yes' : 'no';
+  return cons(question, answer);
+};
+
+export default () => gameFlow(description, primeGame);
